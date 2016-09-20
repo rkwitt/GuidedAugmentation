727c98638e2aed0cd34b425efed0b9a576aeeed4
Roland Kwitt <rkwitt@gmx.at>
2016-09-20 21:50:54 +0200

    ENH: some more testing

--- torch/ae.lua
+++ torch/ae.lua
@@ -6,7 +6,9 @@ require 'nn'
 -- cmdline parsing
 local cmd = torch.CmdLine()
 cmd:option('-logFile', '/tmp/ae.log', 'Logfile')
-cmd:option('-dataFile', '/tmp/data.hdf5', 'HDF5 data file')
+cmd:option('-dataFile', '/tmp/data.hdf5', 'HDF5 source/target training file')
+cmd:option('-predictionFile', '/tmp/prediction.hdf5', 'HDF5 output prediction file')
+cmd:option('-validationFile', '/tmp/val.hdf5', 'HDF5 validation file')
 cmd:option('-learningRate', 0.001, 'Learning rate')
 cmd:option('-epochs', 10, 'Training epochs')
 cmd:option('-batchSize', 300, 'Batchsize')
@@ -14,18 +16,15 @@ local opt = cmd:parse(arg)
 
 -- load some training/testing data
 local fid = hdf5.open(opt.dataFile, 'r')
-local src = fid:read('X_source_trn'):all() -- (training) object activations
-local tst = fid:read('X_source_val'):all() -- (validation) object activations
-local dst = fid:read('X_target'):all() -- (training) target
+local src = fid:read('X_source_trn'):all():transpose(1,2) -- (training-source) object activations
+local dst = fid:read('X_target_trn'):all():transpose(1,2) -- (training-target) object activations
 fid:close()
 
 local N = src:size(1) -- nr. of data points
 local D = src:size(2) -- nr. of dimensions
+assert(N==dst:size(1), 'Whoops...')
 assert(D==dst:size(2), 'Whoops...')
-
-print('#Source (training) data points: '..N..'x'..D)
-print('#Source (validation) data points: '..tst:size(1)..'x'..tst:size(2))
-print('#Target data points: '..dst:size(1)..'x'..dst:size(2))
+print('#Source/Target (training) data points: '..N..'x'..D)
 
 -- logger
 logger = optim.Logger(opt.logFile)
@@ -34,24 +33,24 @@ logger = optim.Logger(opt.logFile)
 local p=0.5
 local ae = nn.Sequential()
 ae:add(nn.Linear(D,512))            -- ENC: dim -> 128
-ae:add(nn.ReLU())                   -- ENC: ReLU
-ae:add(nn.Dropout(p))             -- ENC: dropout
+ae:add(nn.ELU())                    -- ENC: ELU
+ae:add(nn.Dropout(p))               -- ENC: dropout
 
 ae:add(nn.Linear(512,256))          -- ENC: 128 -> 64
-ae:add(nn.ReLU())                   -- ENC: ReLU
-ae:add(nn.Dropout(p))             -- ENC: dropout
+ae:add(nn.ELU())                    -- ENC: ELU
+ae:add(nn.Dropout(p))               -- ENC: dropout
 
-ae:add(nn.Linear(256,64))          -- ENC: 128 -> 64
-ae:add(nn.ReLU())                   -- ENC: ReLU
-ae:add(nn.Dropout(p))             -- ENC: dropout
+ae:add(nn.Linear(256,64))           -- ENC: 128 -> 64
+ae:add(nn.ELU())                    -- ENC: ELU
+ae:add(nn.Dropout(p))               -- ENC: dropout
 
 ae:add(nn.Linear(64,256))           -- DEC: 64 -> 128
-ae:add(nn.ReLU())                   -- DEC: ReLU
-ae:add(nn.Dropout(p))             -- DEC: dropout
+ae:add(nn.ELU())                    -- DEC: ELU
+ae:add(nn.Dropout(p))               -- DEC: dropout
 
 ae:add(nn.Linear(256,512))          -- DEC: 64 -> 128
-ae:add(nn.ReLU())                   -- DEC: ReLU
-ae:add(nn.Dropout(p))             -- DEC: dropout
+ae:add(nn.ReLU())                   -- DEC: ELU
+ae:add(nn.Dropout(p))               -- DEC: dropout
 
 ae:add(nn.Linear(512,D))            -- DEC: 128 -> dim
 ae:add(nn.ReLU())
@@ -91,6 +90,7 @@ local opfunc = function(params)
   return loss, gradTheta
 end
 
+ae:training()
 for epoch = 1, opt.epochs do
   -- random indices for minibatches
   local indices = torch.randperm(N):long():split(opt.batchSize)
@@ -105,8 +105,13 @@ for epoch = 1, opt.epochs do
   end
 end
 
--- TODO: should be done better (e.g., using -test as cmdline option)
-out = ae:forward(tst)
-local out_fid = hdf5.open('/Users/rkwitt/Remote/GuidedAugmentation/python/test/prediction.hdf5', 'w')
-out_fid:write('/prediction', ae:forward(tst))
+
+local fid = hdf5.open(opt.validationFile, 'r')
+local tst = fid:read('X_source_val'):all():transpose(1,2)
+fid:close()
+
+ae:evaluate()
+local out = ae:forward(tst)
+local out_fid = hdf5.open(opt.predictionFile, 'w')
+out_fid:write('/prediction', out)
 out_fid:close()
